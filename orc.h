f2f67d19171af23a10991ef86ae64751ec1ad7bc
Jonathan <jonr79@hotmail.com>
2021-11-02 23:07:54 -0400

    Add ifndef and terminate class

--- orc.h
+++ orc.h
@@ -1,16 +1,17 @@
+#ifndef ORC_H
+#define ORC_H
+
 #include <string>			//For Strings
 #include "Creature.h"			//For base class
 #include "Player.h"
 
+using namespace std;
 
-;using namespace std;
-
-
-
-
-class Orc : public Creature{
+class Orc : public Creature {
 public:
 	Orc();
-	virtual void attack(Player *p2,vector<Item> &pstuff, vector<Item> &stuff,string Map);
+	virtual void attack(Player* p2, vector<Item>& pstuff, vector<Item>& stuff, string Map);
 
-}
+};
+
+#endif
