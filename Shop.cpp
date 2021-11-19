cb8eb94c86ecc25f01a23c9c4e6e5e967e0d0929
Jonathan <jonr79@hotmail.com>
2021-11-19 07:38:26 -0500

    Refactor shop logic

--- Shop.cpp
+++ Shop.cpp
@@ -17,7 +17,6 @@ void Shop::AddItem(Item* item)
 
 void Shop::ShowShop(Player* player, vector<Item*> &playerInventory)
 {
-	int choice = 0;
 	bool escapeWasPressed = false;
 	bool itemSelected = false;
 	bool funds = true;
@@ -26,11 +25,11 @@ void Shop::ShowShop(Player* player, vector<Item*> &playerInventory)
 	PopulateInventory(MapName);
 	unsigned int maxY = Wares.size() + 1;
 
-	map<int, Item*> stuff;
+	map<int, Item*> itemsForSale;
 	
 	for (unsigned int i = 0; i < Wares.size(); i++)
 	{
-		stuff.insert(std::pair<int, Item*>(i + 2, Wares[i]));
+		itemsForSale.insert(std::pair<int, Item*>(i + 2, Wares[i]));
 	}
 
 	while (!escapeWasPressed)
@@ -52,32 +51,37 @@ void Shop::ShowShop(Player* player, vector<Item*> &playerInventory)
 
 		Menu->DrawCursor(cursorPosition, yellow, 175);
 		Display->DisplayItem(Wares[0]);
-		do
+		while (!itemSelected && !escapeWasPressed)
 		{
 			if (Menu->MoveCursor(cursorPosition, itemSelected, escapeWasPressed, 2, maxY))
 			{
-				Menu->DrawCursor(cursorPosition, yellow, 175);
-				choice = cursorPosition.Y;
-				Display->DisplayItem(stuff[choice]);
+				Menu->DrawCursor(cursorPosition, yellow, 175);				
+				Display->DisplayItem(itemsForSale[cursorPosition.Y]);
 			}
 			Display->DisplayText(" ", 79, 23, white);
-		} while (!itemSelected);
+		}
 		if (escapeWasPressed)
 			break;
 
-		choice = cursorPosition.Y;
-		if (player->GetGold() < stuff[choice]->GetCost())
-			funds = false;
-		if (funds)
-		{
-			playerInventory.push_back(stuff[choice]);
-			player->SetGold(player->GetGold() - stuff[choice]->GetCost());
-		}
-		if (!funds)
-		{
-			Display->DisplayText("You do not have sufficient funds", 13, 9, white);
-			Sleep(1500);
-		}
+		BuyItem(player, playerInventory, itemsForSale[cursorPosition.Y]);
 	}// End while bEsc
 	Display->ClearAll();
+}
+
+void Shop::BuyItem(Player* player, std::vector<Item*>& playerInventory, Item* item)
+{
+	bool funds = false;
+
+	if (player->GetGold() < item->GetCost())
+		funds = false;
+	if (funds)
+	{
+		playerInventory.push_back(item);
+		player->SetGold(player->GetGold() - item->GetCost());
+	}
+	if (!funds)
+	{
+		Display->DisplayText("You do not have sufficient funds", 13, 9, white);
+		Sleep(1500);
+	}
 }
