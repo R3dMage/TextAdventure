b518dcac47086956869650d12612a3b256ddd562
Jonathan <jonr79@hotmail.com>
2021-11-25 09:58:00 -0500

    Add methods for saving and loading settings

--- source/ISaveLoadGame.h
+++ source/ISaveLoadGame.h
@@ -5,11 +5,14 @@
 #include "player.h"
 #include "magic.h"
 #include "GameState.h"
+#include "GameSettings.h"
 
 class ISaveLoadGame
 {
 public:
 	virtual void SaveGame(GameState* gameState) = 0;
 	virtual void LoadGame(string filename, GameState* gameState) = 0;
-};
 
+	virtual void SaveSettings(GameSettings* gameSettings) = 0;
+	virtual void LoadSettings(GameSettings* gameSettings) = 0;
+};
